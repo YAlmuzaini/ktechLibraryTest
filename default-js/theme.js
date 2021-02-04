98df1d1e49b5abcb81244cfd9eae37ae09495fe1
Almuzaini <62063512+YAlmuzaini@users.noreply.github.com>
2021-02-04 16:25:47 +0300

    big changes v12

--- default-js/theme.js
+++ default-js/theme.js
@@ -2,3 +2,15 @@ $('#bologna-list a').on('click', function (e) {
     e.preventDefault()
     $(this).tab('show')
 })
+
+
+$('#myTest').on('shown.bs.collapse', function (e) {
+    var parent = document.getElementById("bologna-list");
+    $(parent).removeClass('nav-tabs card-header-tab').addClass('navbar-nav mr-auto pl-3');
+}).on('hidden.bs.collapse', function (e) {
+   $(parent).addClass('nav-tabs card-header-tabs').removeClass('navbar-nav mr-auto pl-3');
+})
+
+$('.nav-tabs>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
