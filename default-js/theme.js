92451384cb73978376fff59842cea070082ef109
Almuzaini <62063512+YAlmuzaini@users.noreply.github.com>
2021-02-13 00:29:40 +0300

    v13

--- default-js/theme.js
+++ default-js/theme.js
@@ -1,16 +1,26 @@
-$('#bologna-list a').on('click', function (e) {
-    e.preventDefault()
-    $(this).tab('show')
-})
+$(function () {
+    var hash = window.location.hash;
+    hash && $('#bologna-list a[href="' + hash + '"]').tab('show');
+
+    $('#bologna-list a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show');
+        var scrollmem = $('body').scrollTop();
+        window.location.hash = this.hash;
+        $('html,body').scrollTop(scrollmem);
+    });
+});
 
 
 $('#myTest').on('shown.bs.collapse', function (e) {
+    e.preventDefault()
     var parent = document.getElementById("bologna-list");
     $(parent).removeClass('nav-tabs card-header-tab').addClass('navbar-nav mr-auto pl-3');
 }).on('hidden.bs.collapse', function (e) {
-   $(parent).addClass('nav-tabs card-header-tabs').removeClass('navbar-nav mr-auto pl-3');
+    e.preventDefault()
+    $(parent).addClass('nav-tabs card-header-tabs').removeClass('navbar-nav mr-auto pl-3');
 })
 
-$('.nav-tabs>a').on('click', function(){
+$('.nav-tabs>a').on('click', function () {
     $('.navbar-collapse').collapse('hide');
 });
